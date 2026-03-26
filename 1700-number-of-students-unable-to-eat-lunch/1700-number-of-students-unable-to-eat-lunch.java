a0f2b3e778e6e2e11bbf026002043e2f7bf7ef0c
DineshDev812 <dk2113536@gmail.com>
2026-03-26 09:17:14 +0530

    Time: 3 ms (14.79%), Space: 43.3 MB (47.45%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -2,10 +2,13 @@ class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         Stack<Integer> sand=new Stack<>();
         Queue<Integer> stu = new LinkedList<>();
+
         for(int i=sandwiches.length-1;i>=0;i--)
         sand.push(sandwiches[i]);
+
         for(int i=0;i<students.length;i++)
         stu.offer(students[i]);
+
         int rej=0;
         while(!stu.isEmpty()&&!sand.isEmpty())
         {
@@ -19,9 +22,10 @@ class Solution {
             {
             stu.offer(stu.poll());
             rej++;
-            }
+            
             if(rej==stu.size())
             break;
+            }
         }
         return stu.size();
 
