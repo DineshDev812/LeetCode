117b42dccfc13502b3d6076d5fe4df02b56a9d05
DineshDev812 <dk2113536@gmail.com>
2026-05-30 14:49:17 +0530

    Time: 4 ms (15.4%), Space: 110.9 MB (39.42%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        Set<Integer> set = new HashSet<>();
-        set.add(edges[0][0]);
-        set.add(edges[0][1]);
+        
+        int a=edges[0][0];
+        int b=edges[0][1];
         int ans =0;
         for(int i=0;i<edges.length;i++)
         {
             for(int j=0;j<edges[0].length;j++)
             {
-                if(set.contains(edges[i][j]))
+                if(a==edges[i][j]||b==edges[i][j])
                 {
                     ans=edges[i][j];
                 }
