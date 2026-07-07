125098d9159c44fe2eee43bf05b3587b7ccf1bf7
DINESH KUMAR R <dk2113536@gmail.com>
2026-07-07 20:41:44 +0530

    Time: 400 ms (19.48%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,5 +2,5 @@ select  a.name
 from Employee a
 inner join Employee b
 on a.id=b.managerId
-where b.managerId is not null
+-- where b.managerId is not null
 group by b.managerId having count(b.managerId)>=5;
