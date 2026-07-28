db9ce8d920fbecd51b71ce417a52ee23e9327821
DINESH KUMAR R <dk2113536@gmail.com>
2026-07-28 21:04:19 +0530

    Time: 1268 ms (5.02%), Space: 46.7 MB (18.78%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=i;j<s.length();j++)
+            {
+                String str=s.substring(i,j+1);
+                StringBuilder sb = new StringBuilder(str);
+                if(str.equals(sb.reverse().toString()))
+                c++;
+
+            }
+        }
+        return c;
+        
+    }
+}
