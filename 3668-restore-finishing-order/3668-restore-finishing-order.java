0cdedf3b1db3ac41bdcaec14c1ca968c527dd5b7
DINESH KUMAR R <dk2113536@gmail.com>
2026-08-17 09:26:20 +0530

    Time: 2 ms (52.66%), Space: 46.7 MB (64.74%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.java
+++ 3668-restore-finishing-order/3668-restore-finishing-order.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
         ArrayList<Integer> friend= new ArrayList<>();
-        ArrayList<Integer> ans= new ArrayList<>();
+
         for(int i:friends)
         friend.add(i);
+        
+        int[] arr= new int[friends.length];
+        int j=0;
         for(int i:order)
         {
             if(friend.contains(i))
-            ans.add(i);
+            arr[j++]=i;
         }
-        int[] arr= new int[ans.size()];
-        int j=0;
-        for(int i:ans)
-        arr[j++]=i;
+      
+       
         return arr;
 
 
