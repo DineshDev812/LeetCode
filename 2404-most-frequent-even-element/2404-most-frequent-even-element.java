c39d7f9cdb4cee0e46db17b8ef8c0a6012b7a1d6
DINESH KUMAR R <dk2113536@gmail.com>
2026-08-28 14:46:10 +0530

    Time: 19 ms (48.64%), Space: 47.5 MB (66.31%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -4,10 +4,11 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int n: nums)
         {
+            if(n%2==0)
             map.put(n,map.getOrDefault(n,0)+1);
         }
         int element=Integer.MAX_VALUE,max=-1;
-         System.out.println(map);
+        //  System.out.println(map);
         for(Map.Entry<Integer,Integer> b:map.entrySet())
         {
             if(b.getKey()%2==0&&b.getValue()>max)
