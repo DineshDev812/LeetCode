51f932951284214111cd3d9bad781a002ed20860
DINESH KUMAR R <dk2113536@gmail.com>
2026-09-08 14:41:42 +0530

    Time: 3 ms (88.66%), Space: 43.2 MB (70.79%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,21 +1,13 @@
 class Solution {
     public int distinctSubseqII(String s) {
-        int n = s.length();
-        int MOD = (int) 1e9 + 7;
-
-        int[] countEndWith = new int[26];
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int idx = s.charAt(i) - 'a';
-
-            int cur = (int) ((1L + sum - countEndWith[idx] + MOD) % MOD);
-
-            sum = (sum + cur) % MOD;
-
-            countEndWith[idx] = (countEndWith[idx] + cur) % MOD;
+        int MAX = 1000000007;
+        long[] end = new long[26];
+        long total = 0;
+        for (char c : s.toCharArray()) {
+            long newEnd = (total + 1) % MAX;
+            total = (total + newEnd - end[c - 'a'] + MAX) % MAX;
+            end[c - 'a'] = newEnd;
         }
-
-        return sum;
+        return (int) total % MAX;
     }
 }
